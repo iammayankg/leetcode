bc85129c928237b6c30d8715d4dde4bff3499919
Mayank Gupta <iammayankg@gmail.com>
2025-11-23 11:51:34 +0530

    Time: 38 ms (59.76%) | Memory: 22.3 MB (27.51%) - LeetSync

--- 1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.py
+++ 1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.py
@@ -1,5 +1,29 @@
 class Solution:
+    def maxSumDivThree2(self, nums: List[int]) -> int:
+        a = sorted([num for num in nums if num % 3 == 0])
+        b = sorted([num for num in nums if num % 3 == 1])
+        c = sorted([num for num in nums if num % 3 == 2])
+
+        total = sum(nums)
+        if total % 3 == 0:
+            return total
+        elif total % 3 == 1:
+            ret = 0
+            if b:
+                ret = max(ret, total - b[0])
+            if len(c) >= 2:
+                ret = max(ret, total - c[0]-c[1])
+            return ret
+        else:
+            ret = 0
+            if c:
+                ret = max(ret, total - c[0])
+            if len(b) >= 2:
+                ret = max(ret, total - b[0] - b[1])
+            return ret
+
     def maxSumDivThree(self, nums: List[int]) -> int:
+        return self.maxSumDivThree2(nums)
         N = len(nums)
         @cache
         def dp(index, mod):
