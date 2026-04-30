eb647ff21ff043631448e1077fb125faf7c76556
Mayank Gupta <iammayankg@gmail.com>
2026-04-30 19:38:05 -0400

    Time: 667 ms (34.58%) | Memory: 72.9 MB (8.23%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -4,16 +4,16 @@ from functools import cache
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         @cache
-        def dp(idx, remaining):
+        def dp(remaining):
             if remaining == 0:
                 return 0
-            if idx == len(coins):
+            if remaining < 0:
                 return math.inf
-            
-            curr_min = dp(idx + 1, remaining)
-            if remaining >= coins[idx]:
-                curr_min = min(curr_min, 1 + dp(idx, remaining-coins[idx]))
+            curr_min = math.inf
+            for coin in coins:
+                if coin <= remaining:
+                    curr_min = min(curr_min, 1 + dp(remaining-coin))
             return curr_min
         
-        ret = dp(0, amount)
+        ret = dp(amount)
         return ret if ret != math.inf else -1
